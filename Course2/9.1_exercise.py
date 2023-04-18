3898cd3df8e7f0d4def649c36e886cad9113b3e4
Yoon Hwan Cho <106636383+YoonHCho@users.noreply.github.com>
2023-04-17 21:24:27 -0700

    started on ch9

---
+++ Course2/9.1_exercise.py
@@ -0,0 +1,22 @@
+''' Exercise 1: 
+Download a copy of the file www.py4e.com/code3/words.txt
+Write a program that reads the words in words.txt and stores them as keys in a dictionary. It doesn’t matter what the values are. Then you
+can use the in operator as a fast way to check whether a string is in the dictionary.
+'''
+
+fname = input("Enter file name: ")
+try:
+    fhandle = open(fname)
+except:
+    print(fname, "doesn't exist")
+    quit()
+
+record = dict()
+
+for line in fhandle:
+    words = line.rstrip().split()
+    for word in words:
+        if word not in record:
+            record[word] = ''
+
+print(record)
